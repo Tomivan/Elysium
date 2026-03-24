280e907c790e2491d73f8bde56b07f021ca88146
Teepremium <gracejunkie20@gmail.com>
2026-03-24 23:30:12 +0100

    added forgot-password page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from "./login/login.component";
 import { CommonModule } from '@angular/common';
 import { ThemeToggleComponent } from "./theme-toggle/theme-toggle.component";
 import { ThemeService } from "./theme-toggle/theme.service";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent, ThemeToggleComponent, CommonModule],
+  imports: [RouterOutlet,  ThemeToggleComponent, CommonModule],
   providers: [ThemeService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
